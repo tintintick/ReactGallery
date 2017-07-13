7326dfcf1d5fc0758bb5da524a8f587e5f9722cc
eric <jacky.gallery@hotmail.com>
2017-07-13 15:58:39 +0800

    add: basic section style

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,38 +4,46 @@ require('styles/App.scss');
 import React from 'react';
 
 let images = require('../data/images');
+(function(){
+  for(var i=0;i<images.length;i++){
+    images[i].path = require("../images/"+images[i].fileName);
+  }
+})();
+
+class ImageFigure extends React.Component {
+
+  render () {
+    return (
+        <figure className="img-figure">
+          <img src={this.props.image.path} alt={this.props.image.title} />
+          <figcaption>
+            <h2 className="img-title">{this.props.image.title}</h2>
+          </figcaption>
+        </figure>
+    );
+  }
+}
 
 class AppComponent extends React.Component {
+
   render() {
-    (function(){
-      for(var i=0;i<images.length;i++){
-        images[i].path = require("../images/"+images[i].fileName);
-      }
-    })();
+    const controllerUnits = [];
     return (
       <div className="index">
         <section className="img-stage">
           <section className="img-sec">
-
+            {
+              images.map(function(item, key) {
+                return (
+                    <ImageFigure image={item} key={key} />
+                );
+              })
+            }
           </section>
           <nav className="controller-nav">
-
+            {controllerUnits}
           </nav>
         </section>
-        {
-          images.map(function(item, key) {
-            return (
-              <div key={key}>
-                <figure>
-                  <img src={item.path} />
-                </figure>
-                <figcaption>
-                  {item.title}
-                </figcaption>
-              </div>
-            )
-          })
-        }
       </div>
     );
   }
