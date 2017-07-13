3bc30284d113628f6b412e39386c3fa021bb792c
eric <jacky.gallery@hotmail.com>
2017-07-13 16:57:42 +0800

    add: init image position

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 let images = require('../data/images');
 (function(){
@@ -26,16 +27,104 @@ class ImageFigure extends React.Component {
 
 class AppComponent extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgsArrangeArr: [
+        // {
+        //   pos: {
+        //     left: '0',
+        //     top: '0'
+        //   }
+        // }
+      ]
+    };
+  }
+
+  Constent = {
+    centerPartPos: {
+      left: 0,
+      top: 0
+    },
+    leftPosRange: {
+      xRange: [0, 0],
+      yRange: [0, 0]
+    },
+    rightPosRange: {
+      xRange: [0, 0],
+      yRange: [0, 0]
+    },
+    topPosRange: {
+      xRange: [0, 0],
+      yRange: [0, 0]
+    }
+  };
+
+  /*
+   * 重新排布图片
+   * @param centerIndex 需要居中图片的ref值
+   */
+  rearrange(centerIndex) {
+
+  }
+
+  componentDidMount() {
+    // 获取图片大小，初始化常量对象
+    let stageNode = ReactDOM.findDOMNode(this.refs.stage),
+        figureNode = ReactDOM.findDOMNode(this.refs.imgFigure0),
+        stageW = stageNode.scollWidth,
+        stageH = stageNode.scollHeight,
+        halfStageW = Math.ceil(stageW / 2),
+        halfStageH = Math.ceil(stageH / 2),
+        figureW = figureNode.scollWidth,
+        figureH = figureNode.scollHeight,
+        halfFigureW = Math.ceil(figureW / 2),
+        halfFigureH = Math.ceil(figureH / 2);
+
+    // 中间图片坐标
+    this.Constent.centerPartPos = {
+      left: halfStageW-halfFigureW,
+      top: halfStageH-halfFigureH
+    };
+    // 左部图片坐标取值范围
+    this.Constent.leftPosRange = {
+      xRange: [-halfFigureW, halfStageW-halfFigureW*3],
+      yRange: [-halfFigureH, stageH-halfFigureH]
+    };
+    // 右部图片坐标取值范围
+    this.Constent.rightPosRange = {
+      xRange: [halfStageW+halfFigureW, stageW-halfFigureW],
+      yRange: [-halfFigureH, stageH-halfFigureH]
+    };
+    // 上部图片坐标取值范围
+    this.Constent.topPosRange = {
+      xRange: [halfStageW-figureW, halfStageW],
+      yRange: [halfStageH+halfFigureH, stageH-halfFigureH]
+    };
+
+    // 默认指定第一张图片居中
+    this.rearrange(0);
+  }
+
   render() {
     const controllerUnits = [];
     return (
       <div className="index">
-        <section className="img-stage">
+        <section className="img-stage" ref="stage">
           <section className="img-sec">
             {
-              images.map(function(item, key) {
+              images.map((item, index) => {
+                // 为每张图片初始化state
+                if(!this.state.imgsArrangeArr[index]) {
+                  this.state.imgsArrangeArr[index] = {
+                    pos: {
+                      left: '0',
+                      top: '0'
+                    }
+                  }
+                }
                 return (
-                    <ImageFigure image={item} key={key} />
+                    <ImageFigure image={item} ref={'imgFigure'+index} key={index} />
                 );
               })
             }
